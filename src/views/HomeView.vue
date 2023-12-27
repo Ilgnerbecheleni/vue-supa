097a281e9e01502afe8f3533ecd4ae0c05b046f7
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2023-12-27 19:49:02 +0000

    iniciando vue e supabase

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { supabase } from '../../supabase';
+import { onMounted, ref } from 'vue';
+
+const nomes = ref([]);
+const listar = async ()=>{
+  
+let { data: agenda, error } = await supabase
+  .from('agenda')
+  .select('*')
+    nomes.value = agenda
+    console.log(agenda)
+  }
+
+  onMounted(() => {
+    listar()
+  })
+
+
+const msg = import.meta.env.VITE_SOME_KEY;
+console.log(msg);
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li v-for="nome in nomes" :key="nome.id">{{ nome.nome }}</li>
+    </ul>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const user = ref({email:'' , senha:''});
+
+const handlesubmit = ()=>{
+ console.log(user.value);
+user.value.email = ''
+user.value.senha = ''
+    }
+
+
+</script>
+
+<template>
+<form   @submit.prevent="handlesubmit">
+<input type="text" v-model="user.email">
+<input type="password" v-model="user.senha">
+<input type="submit" value="entrar">
+</form>
+</template>
